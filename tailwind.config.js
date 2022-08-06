411fa8b00d61b9467d29deeef1fb106f6fafe41d
Nworie Kingsley <obizzkingsley@gmail.com>
2022-08-06 20:26:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {colors: {
+      'main': '#234566'
+    },},
+  },
+  plugins: [],
+}
